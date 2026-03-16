bcf3af28c1f270f3196d5658c2bcea596270461c
Oeshen Nix <oeshennix@gmail.com>
2026-03-15 21:18:39 -0500

    corrected flake for the nix-helper

--- other/nix-helper/default.nix
+++ other/nix-helper/default.nix
@@ -1,4 +1,4 @@
-{pkgs?(import <nixpkgs>(builtins.currentSystem)}:pkgs.stdenv.mkDerivation{
+{pkgs?(import <nixpkgs>(builtins.currentSystem))}:pkgs.stdenv.mkDerivation{
   name="neovim-nix-helper";
 
   version="0.1.0";
@@ -7,4 +7,3 @@
   buildPhase = "gcc -O2 -s $src -o nixneovimmanager";
   installPhase = "mkdir -p $out/bin; mv nixneovimmanager $out/bin/nix-helper";
 }
-
