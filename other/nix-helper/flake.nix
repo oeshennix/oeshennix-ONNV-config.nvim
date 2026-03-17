5d62404d3e72b6c0d992b0a0f9ca34fbf0dde512
Oeshen Nix <oeshennix@gmail.com>
2026-03-17 11:31:23 -0500

    edit nix flake to work on other systems

--- other/nix-helper/flake.nix
+++ other/nix-helper/flake.nix
@@ -1,14 +1,29 @@
 {
-  description = "A very basic flake";
+  description = "neovim-nix-helper - Tiny C wrapper for nix commands used by ONNV";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: let
-    system="x86_64-linux";
-    pkgs=import nixpkgs{inherit system;};
-  in{
-    packages.x86_64-linux.default = import ./default.nix{inherit pkgs;};
-  };
+  outputs = { self, nixpkgs }:
+    let
+      # Supported systems - add more if you ever need them
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      # Helper function to generate packages for every system
+      forEachSystem = f:
+        nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
+    in
+    {
+      packages = forEachSystem (pkgs:
+        {
+          default = import ./default.nix { inherit pkgs; };
+        }
+      );
+    };
 }
