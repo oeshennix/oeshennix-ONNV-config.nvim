9e5f6f33630c562f1aa3b2a3b19ad248c31a101e
oeshennix <oeshennix@gmail.com>
2026-07-11 14:12:03 -0400

    changed env for changed nvim-ONNV-config.nvim

--- lua/nvimonnvconfig/modules/env.lua
+++ lua/nvimonnvconfig/modules/env.lua
@@ -1,7 +1,7 @@
 local ONNVmorph=require("ONNV.morph")
 local M={}
 function M.run(baseconfig,callback)
-  local config=baseconfig.env;
+  local config=baseconfig.env or {};
   ONNVmorph.morph(config,baseconfig,{"concat","var"});
   for c,v in pairs(config)do
     vim.env[c]=v;
